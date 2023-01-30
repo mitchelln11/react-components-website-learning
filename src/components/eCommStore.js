bf8ab05988030189a23aae9033302bc9dcbead35
Mitchell Naleid <mitchellnaleid@gmail.com>
2023-01-29 21:12:35 -0600

    functioning sets of checkboxes with change logic

--- src/components/eCommStore.js
+++ src/components/eCommStore.js
@@ -13,9 +13,18 @@ function DisplayProducts({id, productName, price, image}) {
 }
 
 export default function ProductGallery() {
+    // const baseCheckbox = document.getElementById('#product-store > .checkbox-item > input')
+    // switch(baseCheckbox + checkVal){
+    //     case 'Merrell':
+    //     productList.filter((product) => {
+    //         return (
+    //             <DisplayProducts key={product.id} productName={product.productName} price={product.price} image={product.image || placeholderImg} id={product.id} />
+    //         )
+    //     })  
+    //     break;
+    // }
     return (
         // Maybe put filter here instead of map. Leave below as the else statement
-        
         productList.map((product) => {
             return (
                 <DisplayProducts key={product.id} productName={product.productName} price={product.price} image={product.image || placeholderImg} id={product.id} />
