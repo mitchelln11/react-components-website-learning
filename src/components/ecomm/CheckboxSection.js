8b7527cc98cc5ebfbf2230bd09156b1e30d281cf
Mitchell Naleid <mitchellnaleid@gmail.com>
2023-04-04 21:57:20 -0500

    fix: remove unused checkbox component

--- src/components/ecomm/CheckboxSection.js
+++
@@ -1,9 +0,0 @@
-import { eCommValues, GenerateCheckboxList } from './CheckBoxEcommParent';
-
-// Display all options without duplicates
-export default function GenericCheckboxList({prodVal}) {
-    return (
-        eCommValues[prodVal].map((item, index) => {
-            return <GenerateCheckboxList key={index} checkboxValue={item} data-product-element={prodVal} />
-    })
-)}
