6d931cd056fdf53998347b0d5b5201e4b10b7f0b
Mitchell Naleid <mitchellnaleid@gmail.com>
2023-06-02 23:01:32 -0500

    fix: testing github push

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -2,6 +2,7 @@ export default function Products() {
     return (
         <div id="products">
             <p>Just checking to see if this gets to the products page. Still need to figure out the product id which is on the documentation.</p>
+            <p>Not a real thing, just checking because of Github issues.</p>
         </div>
     )
 }
