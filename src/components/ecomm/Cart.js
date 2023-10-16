eec4407af4ad9cc7afe29c00709a9205676f7abe
Mitchell Naleid <mitchellnaleid@gmail.com>
2023-10-15 21:56:00 -0500

    feature: cart page

---
+++ src/components/ecomm/Cart.js
@@ -0,0 +1,31 @@
+
+
+export function CartItem() {
+    return(
+        <div id="cart-page">
+            <div className="cart-row-container">
+                <div className="cart-item">
+                    <div className="item-details">
+                        {/* <img src={productList[id].image || placeholderImg} alt={productList[id].productName} /> */}
+                        <img src={require('../../images/merrell-moab-mens-hiking-boot.jpg')} alt="Merrell Moab Men's Hiking Boot" />
+                        <h2>Merrell Moab Men's Hiking Boot</h2>
+                    </div>
+                    <div className="price-details">
+                        <p>$65.00</p>
+                        <p>Sale: $55.00</p>
+                    </div>
+                </div>
+            {/* Potential items to add to page */}
+            {/* Quantity checkbox */}
+            {/* Delete option */}
+            {/* Go to item on click */}
+            </div>
+
+            {/* Shipping info Sidebar */}
+            <aside id="cart-sidebar" className="primary-color primary-font">
+                <p>Shipping:</p>
+                <p>Tax</p>
+            </aside>
+        </div>
+    )
+}
