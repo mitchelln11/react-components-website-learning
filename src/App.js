4cd91ff335b41bbd57abf9ce5b8bb887e1723966
Mitchell Naleid <mitchellnaleid@gmail.com>
2023-10-19 22:47:29 -0500

    feature: add functioning modal on button click with useContext

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 import './App.css';
+import { createContext, useState } from 'react';
 import Accordion from './components/accordion';
 import { StoreSection } from './components/ecomm/eCommStore';
 import FormFill from './components/formFill';
-import { ModalBtn, ModalPopUp } from './components/ModalPopUp';
+import { ModalPopUp, ModalBtn } from './components/ModalPopUp';
+
+export const ModalContext = createContext();
 
 function App() {
+  const [revealModal, setRevealModal] = useState(false);
   return (
-    <div className="App">
+    <ModalContext.Provider value={[revealModal, setRevealModal]}>
       <ModalPopUp />
-      {/* A static component that displays static text */}
-      <h2>Historical Facts</h2>
-      <div id="accordionSection">
-        <p>The following accordion is pieced together with basic elements of React. The data, out of simplicity and the goal of learning React, is pulling from an array of objects within the file structure as opposed to pulling from an actual database.</p>
-        <Accordion />
-      </div>
-      <h2>Shop From Our Store</h2>
-        <p>This small eCommerce store is built out of React components. Rather than using the more advanced <i>useContext</i> or a third party such as <i>Redux</i>, I used the practice of "Lifting state up" that helps adjacent components work together. This data, again out of simplicity, is pulling from another array of objects within the file structure.</p>
-        <StoreSection />
+      <div className="App">
+        
+        {/* A static component that displays static text */}
+        <h2>Historical Facts</h2>
+        <div id="accordionSection">
+          <p>The following accordion is pieced together with basic elements of React. The data, out of simplicity and the goal of learning React, is pulling from an array of objects within the file structure as opposed to pulling from an actual database.</p>
+          <Accordion />
+        </div>
+        <h2>Shop From Our Store</h2>
+          <p>This small eCommerce store is built out of React components. Rather than using the more advanced <i>useContext</i> or a third party such as <i>Redux</i>, I used the practice of "Lifting state up" that helps adjacent components work together. This data, again out of simplicity, is pulling from another array of objects within the file structure.</p>
+          <StoreSection />
 
-      <div id="yt-vids">
-        <h2>Youtube Videos</h2>
-        <ModalBtn />
+        <div id="yt-vids">
+          <h2>Youtube Videos</h2>
+          <ModalBtn />
+        </div>
+        <h2>Contact Us</h2>
+        <div id="form-fill">
+          <FormFill />
+        </div>
       </div>
-      <h2>Contact Us</h2>
-      <div id="form-fill">
-        <FormFill />
-      </div>
-    </div>
+    </ModalContext.Provider>
   );
 }
 
--- src/components/ModalPopUp.js
+++ src/components/ModalPopUp.js
@@ -1,26 +1,37 @@
-import { useState, useContext, createContext } from "react";
-
-const ModalContext = createContext(false);
-
-export function ModalBtn() {
-    const [revealModal, setRevealModal] = useState(false);
-    return(
-        <ModalContext.Provider value={revealModal}>
-            <div className="modal-btn" onClick={() => {setRevealModal(true)}}>Btn Test</div>
-        </ModalContext.Provider>
-    )
-}
+import { useContext, useEffect } from "react";
+import { ModalContext } from '../App';
 
 export function ModalPopUp() {
-    const { revealModal, setRevealModal } = useContext(ModalContext);
+    const [ revealModal, setRevealModal ] = useContext(ModalContext);
+
+    useEffect(() => {
+        console.log(revealModal)
+    })
+
+    function handleCloseModal () {
+        setRevealModal(false)
+    }
+
     if (revealModal === true) {
         return (
             <div className="modal">
                 <div className="inner-container">
                     <h2>Modal Test</h2>
-                    <div className="modal-btn" onClick={() => {setRevealModal(false)}}>Test Close button</div>
+                    <div className="modal-btn" onClick={handleCloseModal}>Test Close button</div>
                 </div>
             </div>
         )
     }
+}
+
+export function ModalBtn() {
+    const [ , setRevealModal ] = useContext(ModalContext);
+
+    function handleOpenModal () {
+        setRevealModal(true)
+    }
+
+    return(
+        <div className="modal-btn" onClick={handleOpenModal}>Btn Test</div>
+    )
 }
