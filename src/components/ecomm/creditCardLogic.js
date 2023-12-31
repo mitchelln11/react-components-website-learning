8a19c64604afd79d9ebc7f9f8e8520c611584a6e
Mitchell Naleid <mitchellnaleid@gmail.com>
2023-12-30 21:12:41 -0600

    feature: add cleave-zen for Credit card formatting

--- src/components/ecomm/creditCardLogic.js
+++ src/components/ecomm/creditCardLogic.js
@@ -1,54 +1,49 @@
-// const ccRegex = /[0-9]|\./;
+import { useState, useRef } from "react";
+import { formatCreditCard, getCreditCardType } from "cleave-zen";
 
-// export function pullCcInputValue() {
-//     // console.log(document.getElementById("cc-field").value)
-//     // may have to set up an input state first to get full value
-//     return document.getElementById("cc-field").value
-// }
-
-export function parseCcForType(ccNum) {
-    switch (true) {
-        case ccNum.charAt(0) === "2":
-            return "Mastercard"
-        case ccNum.charAt(0) === "3":
-            return "American Express"
-        case ccNum.charAt(0) === "4":
-            return "Visa"
-        case ccNum.charAt(0) === "5":
-            return "Mastercard"
-        case (ccNum.charAt(0) === "6"):
-            return "Discover"
-        default: 
-            return "No Credit Card Match"
-    }
-}
-
-// export function determineCcCompany () {
-//     // still have to determine which numbers mean which CC company (Switch Case?)
-//     // Do I need a separate function that takes in a cc number and parses through?
-//     // if 1st keypress is *, remove * from the options
-//       // 3 = American Express, 4 = Visa, 2,5 = Mastercard, 6 = Discover, 7 = Gas Card, 9 = government
-//     // If 2nd keypress, remove * from options
-//     // Display CC company
-
-//     switch(x) {
-//         case y:
-//             // code
-//             break;
-//         case z:
-//             // code
-//             break;
-//         default:
-//             // code
+// export function parseCcForType(ccNum) {
+//     switch (true) {
+//         case ccNum.charAt(0) === "2":
+//             return "Mastercard"
+//         case ccNum.charAt(0) === "3":
+//             return "American Express"
+//         case ccNum.charAt(0) === "4":
+//             return "Visa"
+//         case ccNum.charAt(0) === "5":
+//             return "Mastercard"
+//         case (ccNum.charAt(0) === "6"):
+//             return "Discover"
+//         default: 
+//             return "No Credit Card Match"
 //     }
 // }
 
-// export default function CcLogic({fieldId}) {
-//     // Limit field to numbers
-//     // require specific number of characters
-//     // remove number arrows
-//     pullCcInputValue()
-//     return (
-//         <input id={fieldId} name={fieldId} type="text" inputMode="numeric" pattern="[0-9]+" placeholder="Ex: 4502783790218767" />
-//     )   
-// }
+export default function CcLogic({fieldId}) {
+    const inputRef = useRef(null)
+    const [ ccValue, setCcValue ] = useState('');
+    const [ ccType, setCctype ] = useState('');
+
+    const handleCcChange = (event) => {
+        const value = event.target.value;
+        setCcValue(formatCreditCard(value));
+        setCctype(getCreditCardType(value));
+    }
+    
+    return (
+        <>
+            <input 
+                ref={inputRef}
+                value={ccValue}
+                id={fieldId} 
+                name={fieldId} 
+                onChange={handleCcChange} 
+                maxlength="19" 
+                type="text" 
+                inputmode="numeric" 
+                pattern="[0-9]{4} [0-9]{4} [0-9]{4} [0-9]{4}" 
+                placeholder="Ex: 4502783790218767" 
+            />
+            <div>type: {ccType} </div>
+        </>
+    )   
+}
